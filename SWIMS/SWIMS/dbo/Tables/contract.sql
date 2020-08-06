cd48f3101ce819c054cff1ad7b8cf246a2cabe45
Steve Wall <weavetall@hotmail.com>
2020-08-06 14:31:11 +0100

    DB Changes

---
+++ SWIMS/SWIMS/dbo/Tables/contract.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[contract] (
+    [contract_id]            NVARCHAR (50) NOT NULL,
+    [contract_desc]          NVARCHAR (50) NULL,
+    [contract_canimport]     NVARCHAR (50) NULL,
+    [contract_inactive]      BIGINT        CONSTRAINT [InactiveYesOrNo] DEFAULT ((0)) NOT NULL,
+    [contract_taskvaluation] SMALLINT      CONSTRAINT [Value1to10] DEFAULT ((0)) NULL,
+    [contract_nillrevenue]   INT           CONSTRAINT [value-1000to1000] DEFAULT ((0)) NULL,
+    CONSTRAINT [PK_contract] PRIMARY KEY NONCLUSTERED ([contract_id] ASC)
+);
+
---
+++ SWIMS/SWIMS/dbo/Tables/job.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[job] (
+    [job_id]         UNIQUEIDENTIFIER NOT NULL,
+    [application_id] NVARCHAR (50)    NULL,
+    [contract_id]    NVARCHAR (50)    NOT NULL,
+    [district_id]    NVARCHAR (50)    NULL,
+    [masterjob_id]   INT              NULL,
+    [job_address]    NVARCHAR (255)   NULL,
+    CONSTRAINT [PK_job] PRIMARY KEY NONCLUSTERED ([job_id] ASC)
+);
+
