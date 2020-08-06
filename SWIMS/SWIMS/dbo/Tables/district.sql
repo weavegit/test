5d641909639753b6ef51b9704522c223d44d77aa
Steve Wall <weavetall@hotmail.com>
2020-08-06 17:06:08 +0100

    DB changes to structure

--- SWIMS/SWIMS/dbo/Tables/district.sql
+++ SWIMS/SWIMS/dbo/Tables/district.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[district] (
-    [district_id]       NVARCHAR (50)  NOT NULL,
-    [contract_id]       NVARCHAR (50)  NOT NULL,
+    [district_id] INT NOT NULL,
+    [contract_id] INT NOT NULL,
+    [district_code]       NVARCHAR (50)  NOT NULL,
     [district_desc]     NVARCHAR (50)  NULL,
     [district_address]  NVARCHAR (255) NULL,
     [district_postcode] NVARCHAR (12)  NULL,
-    [district_primary]  INT            CONSTRAINT [IsPrimaryYesOrNo] DEFAULT ((0)) NULL,
-    CONSTRAINT [PK_district] PRIMARY KEY NONCLUSTERED ([district_id] ASC, [contract_id] ASC)
+    [district_primary]  INT CONSTRAINT [IsPrimaryYesOrNo] DEFAULT ((0)) NULL, 
+    CONSTRAINT [PK_district] PRIMARY KEY ([district_id]), 
+    CONSTRAINT [FK_district_To_Contract] FOREIGN KEY ([contract_id]) REFERENCES [contract]([contract_id])
 );
 
--- SWIMS/SWIMS/dbo/Tables/job.sql
+++ SWIMS/SWIMS/dbo/Tables/job.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[job] (
-    [job_id]         UNIQUEIDENTIFIER NOT NULL,
+    [job_id]         INT NOT NULL,
     [application_id] NVARCHAR (50)    NULL,
-    [contract_id]    NVARCHAR (50)    NOT NULL,
-    [district_id]    NVARCHAR (50)    NULL,
+    [contract_id]    INT    NOT NULL,
+    [district_id]    INT    NOT NULL,
     [masterjob_id]   INT              NULL,
     [job_address]    NVARCHAR (255)   NULL,
-    CONSTRAINT [PK_job] PRIMARY KEY NONCLUSTERED ([job_id] ASC)
+    CONSTRAINT [PK_job] PRIMARY KEY NONCLUSTERED ([job_id] ASC), 
+    CONSTRAINT [FK_job_To_Contract] FOREIGN KEY  ([contract_id]) REFERENCES [contract]([contract_id]), 
+    CONSTRAINT [FK_job_To_District] FOREIGN KEY  ([district_id]) REFERENCES [district]([district_id])
 );
 
